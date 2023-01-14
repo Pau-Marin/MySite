a3e44d157578f90404e6d1c759161cdbe37106a9
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2023-01-14 18:43:05 +0100

    Home page with some info

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,24 @@
+import profilePicture from "../assets/img/profilePicture.jpg" //"../assets/img/Pau.jpg"
+import styles from "../styles/Home.module.css"
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam
-        obcaecati, molestias doloremque necessitatibus sint deserunt distinctio
-        quas sapiente, explicabo enim illo itaque, aut cum. Expedita voluptatem
-        soluta aspernatur blanditiis praesentium?
-      </p>
+      <div className={styles.container}>
+        <img
+          src={profilePicture.src}
+          alt="Mi foto"
+          className={styles.profilePicture}
+        />
+        <p className={styles.text}>
+          ¡Buenas! Me llamo Pau y soy un desarrollador web apasionado. He estado
+          escribiendo código durante más de 10 años y he trabajado con varios
+          lenguajes y herramientas. Actualmente, estoy enfocado en el desarrollo
+          web con React y NextJS. Si estás interesado en ver algunos de mis
+          proyectos, ¡dale un vistazo a mi portfolio!
+        </p>
+      </div>
     </div>
   )
 }
