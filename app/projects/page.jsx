2e75c1b2df4459611a8014d252686712f5878bee
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2023-01-14 18:42:13 +0100

    Projects page with placeholder info

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,13 +1,32 @@
-export default function page() {
+export default function Projects() {
   return (
     <div>
       <h1>Projects</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam
-        obcaecati, molestias doloremque necessitatibus sint deserunt distinctio
-        quas sapiente, explicabo enim illo itaque, aut cum. Expedita voluptatem
-        soluta aspernatur blanditiis praesentium?
-      </p>
+      <ul>
+        <li>
+          <h2>Sitio web de comercio electrónico</h2>
+          <p>
+            Desarrollé un sitio web de comercio electrónico para una pequeña
+            empresa utilizando React y Node.js.
+          </p>
+          <a href="https://ejemplo.com">Ver en vivo</a>
+        </li>
+        <li>
+          <h2>Aplicación móvil de citas</h2>
+          <p>
+            Colaboré en el desarrollo de una aplicación móvil de citas
+            utilizando React Native y Firebase.
+          </p>
+          <a href="https://ejemplo.com">Ver en vivo</a>
+        </li>
+        <li>
+          <h2>Sistema de gestión de contenidos</h2>
+          <p>
+            Desarrollé un sistema de gestión de contenidos para una organización
+            sin fines de lucro utilizado...
+          </p>
+        </li>
+      </ul>
     </div>
   )
 }
