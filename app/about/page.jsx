3600951e83538e90f6e3e03f0afca8676b83091c
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2023-01-15 01:09:22 +0100

    Basic timeline for About page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -6,30 +6,55 @@ export default function About() {
       <div className={styles.main}>
         <h1>About</h1>
         <p>
-          Soy un desarrollador web con experiencia en construir sitios web y
-          aplicaciones móviles utilizando tecnologías como React, Node.js y
-          MongoDB.
+          ¡Hola! Me llamo Pau y soy un desarrollador web apasionado con más de
+          10 años de experiencia. Me especializo en el desarrollo web con React
+          y NextJS, pero también tengo experiencia en otras tecnologías como
+          HTML, CSS, JavaScript y C#. He trabajado en una variedad de proyectos,
+          desde pequeñas aplicaciones web hasta proyectos de gran escala con
+          equipos de desarrollo. Mi experiencia también incluye gestión de
+          cambios en una empresa importante. En mi tiempo libre, me gusta seguir
+          aprendiendo nuevas habilidades y tecnologías para mejorar como
+          desarrollador.
         </p>
       </div>
+      <hr></hr>
       <div className={styles.timeline}>
-        <ul>
-          <li>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam
-              obcaecati, molestias doloremque necessitatibus sint deserunt
-              distinctio quas sapiente, explicabo enim illo itaque, aut cum.
-              Expedita voluptatem soluta aspernatur blanditiis praesentium?
-            </p>
-          </li>
-          <li>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, illo
-              magnam similique rem possimus nostrum nobis beatae incidunt
-              dignissimos aliquid obcaecati cupiditate at molestiae placeat
-              quasi qui in consequuntur. Dolor.
-            </p>
-          </li>
-        </ul>
+        <div className={`${styles.timelineEvent} ${styles.leftTimelineEvent}`}>
+          <img src="https://media.licdn.com/dms/image/C4D0BAQG6fJDrblcY9A/company-logo_200_200/0/1656698101163?e=1681948800&v=beta&t=ustOMbEDv6vcB-KMKrYidYUzLYK3jwoFVXqigQocEo8" />
+          <div className={styles.timelineDescription}>
+            <h2>Henry</h2>
+            <h5>2022-2023</h5>
+            <p>Bootcamp FullStack development</p>
+            <span className={styles.leftArrow}></span>
+          </div>
+        </div>
+        <div className={`${styles.timelineEvent} ${styles.rightTimelineEvent}`}>
+          <img src="https://media.licdn.com/dms/image/C4E0BAQHdMzoyvpdZPQ/company-logo_200_200/0/1604936695808?e=1681948800&v=beta&t=SZHTX4D7iXKcFD35yvtQaVbQcvw_0lTXDEokyuahU00" />
+          <div className={styles.timelineDescription}>
+            <h2>Angel Swing Studio</h2>
+            <h5>2020-Actualidad</h5>
+            <p>Emprendedor</p>
+            <span className={styles.rightArrow}></span>
+          </div>
+        </div>
+        <div className={`${styles.timelineEvent} ${styles.leftTimelineEvent}`}>
+          <img src="https://media.licdn.com/dms/image/C4D0BAQHL2aI80-B_KQ/company-logo_200_200/0/1519896333884?e=1681948800&v=beta&t=7HCyGJZEb5P9IMTRO1YzTvLPR6pIO9aJXUqKk5sCkbk" />
+          <div className={styles.timelineDescription}>
+            <h2>R&C Associats</h2>
+            <h5>2019-2020</h5>
+            <p>Jefe de sala</p>
+            <span className={styles.leftArrow}></span>
+          </div>
+        </div>
+        <div className={`${styles.timelineEvent} ${styles.rightTimelineEvent}`}>
+          <img src="https://media.licdn.com/dms/image/C4D0BAQHL2aI80-B_KQ/company-logo_200_200/0/1519896333884?e=1681948800&v=beta&t=7HCyGJZEb5P9IMTRO1YzTvLPR6pIO9aJXUqKk5sCkbk" />
+          <div className={styles.timelineDescription}>
+            <h2>R&C Associats</h2>
+            <h5>2017-2019</h5>
+            <p>Operador Batch</p>
+            <span className={styles.rightArrow}></span>
+          </div>
+        </div>
       </div>
     </div>
   )
