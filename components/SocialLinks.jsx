3fc0d52b9ad9f2d412c0325da097be5eebe1b4e6
Pau-Marin <ritsu95@icloud.com>
2023-02-01 17:16:46 +0100

    Lateral social links

---
+++ components/SocialLinks.jsx
@@ -0,0 +1,58 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa"
+import { HiOutlineMail } from "react-icons/hi"
+import { BsFillPersonLinesFill } from "react-icons/bs"
+
+export default function SocialLinks() {
+  const socials = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={30} />
+        </>
+      ),
+      url: "https://www.linkedin.com/in/pau-marin/",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={30} />
+        </>
+      ),
+      url: "https://github.com/Pau-Marin",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      url: "mailto:pau.marin.jubete@gmail.com",
+      style: "rounded-br-md",
+    },
+  ]
+
+  return (
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        {socials.map(({ id, child, url, style }) => (
+          <li
+            className={`flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 ${style}`}
+          >
+            <a
+              href={url}
+              className="flex justify-between items-center w-full text-white"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
