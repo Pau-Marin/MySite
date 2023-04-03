9f9414ca55bab129e82e7692a6ed7d872293326e
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2023-04-04 00:16:54 +0200

    Added actual projects

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,41 +1,48 @@
+import projectEmme3d from "../assets/img/portfolio/project-emme3d.png"
+import projectPikemon from "../assets/img/portfolio/project-pikemon.png"
+import projectPortfolio from "../assets/img/portfolio/project-portfolio.png"
+
 export default function Portfolio() {
   const projects = [
     {
       id: 1,
-      img: "",
-      deploy: "",
-      github: "",
+      name: "Portfolio Site",
+      img: projectPortfolio.src,
+      deploy: "/",
+      github: "https://github.com/Pau-Marin/Portfolio-Site/",
     },
     {
       id: 2,
-      img: "",
-      deploy: "",
-      github: "",
+      name: "Emme-3D",
+      img: projectEmme3d.src,
+      deploy: "https://emme-3-d.vercel.app",
+      github: "https://github.com/MacarenaRodriguez1993/Emme-3D",
     },
     {
       id: 3,
-      img: "",
-      deploy: "",
-      github: "",
+      name: "PIkemon",
+      img: projectPikemon.src,
+      deploy: "https://pikemon.ddns.net",
+      github: "https://github.com/Pau-Marin/PI-Pokemon",
     },
-    {
-      id: 4,
-      img: "",
-      deploy: "",
-      github: "",
-    },
-    {
-      id: 5,
-      img: "",
-      deploy: "",
-      github: "",
-    },
-    {
-      id: 6,
-      img: "",
-      deploy: "",
-      github: "",
-    },
+    // {
+    //   id: 4,
+    //   img: "",
+    //   deploy: "",
+    //   github: "",
+    // },
+    // {
+    //   id: 5,
+    //   img: "",
+    //   deploy: "",
+    //   github: "",
+    // },
+    // {
+    //   id: 6,
+    //   img: "",
+    //   deploy: "",
+    //   github: "",
+    // },
   ]
 
   return (
@@ -43,16 +50,17 @@ export default function Portfolio() {
       name="projects"
       className="bg-gradient-to-b from-black to-gray-800 w-full text-white"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-auto">
+      <div className="max-w-screen-lg mt-20 p-4 mx-auto flex flex-col justify-center w-full h-auto">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
           </p>
-          <p className="py-6">Check out some of my work right here</p>
+          <p className="py-6">Check out some of my work :)</p>
         </div>
         <div className="w-full h-auto  grid md:grid-cols-3 gap-8 px-12 sm:grid-cols-2 sm:px0 xs:grid-cols-1">
-          {projects.map(({ id, img, deploy, github }) => (
+          {projects.map(({ id, name, img, deploy, github }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <h3 className="text-center text-xl">{name}</h3>
               <img
                 src={img}
                 alt=""
