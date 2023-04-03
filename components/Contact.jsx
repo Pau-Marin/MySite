09036bcb3eb399b5d641efe701db644873a86605
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2023-04-03 22:47:12 +0200

    Update Contact.jsx
    Commented form, will make it work later

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -4,14 +4,18 @@ export default function Contact() {
       name="contact"
       className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
     >
-      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-auto">
+      <div className="flex flex-col mt-20 p-4 justify-center max-w-screen-lg mx-auto h-auto">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
           </p>
-          <p className="py-6">Submit the form</p>
+          <p className="py-6">
+            Send me an email:{" "}
+            <a href={"mailto:hello@paumarin.com"}>hello@paumarin.com</a>
+          </p>
+          {/* <p className="py-6">Let's talk</p> */}
         </div>
-        <div className="flex justify-center items-center">
+        {/* <div className="flex justify-center items-center"> 
           <form action="" className="flex flex-col w-full md:w-1/2">
             <input
               type="text"
@@ -32,10 +36,10 @@ export default function Contact() {
               className="p-1 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
             <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
-              Let's talk
+              Send!
             </button>
           </form>
-        </div>
+  </div>*/}
       </div>
     </div>
   )
