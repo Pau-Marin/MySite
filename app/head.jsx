cc7be7e3eab8faf82b7ead63980245e4e1734b35
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2023-04-04 23:52:46 +0200

    Update head.jsx
    Updated the title that's shown when the site is shared on Social Networks.

--- app/head.jsx
+++ app/head.jsx
@@ -5,6 +5,7 @@ export default function Head({ title }) {
     <>
       <title>{title}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta property="og:title" content={title} />
       <meta property="og:image" content={projectPortfolio.src} />
       <link rel="icon" href="/favicon.ico" />
       {/* <style>{"* {border: 1px solid red;}"}</style> */}
