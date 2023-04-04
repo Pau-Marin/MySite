b6b9c14f4e2f82991b44a3258dbbc5803cfef3a2
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2023-04-04 23:09:30 +0200

    Update head.jsx
    Updated the image that's shown when the site is shared on Social Networks.

--- app/head.jsx
+++ app/head.jsx
@@ -1,8 +1,11 @@
+import projectPortfolio from "../assets/img/portfolio/project-portfolio.png"
+
 export default function Head({ title }) {
   return (
     <>
       <title>{title}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta property="og:image" content={projectPortfolio.src} />
       <link rel="icon" href="/favicon.ico" />
       {/* <style>{"* {border: 1px solid red;}"}</style> */}
     </>
