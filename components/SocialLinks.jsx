f58177247eb0cf27dbc8a80779af457e8b729a08
Pau Marin <ritsu95@icloud.com>
2023-04-09 12:51:17 +0200

    Refactor cleanup (#10)
    * Refactor
    
    Cleaned some unused imports
    
    * Update SocialLinks.jsx
    
    Added a key for each child on the list.

--- components/SocialLinks.jsx
+++ components/SocialLinks.jsx
@@ -1,6 +1,5 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa"
 import { HiOutlineMail } from "react-icons/hi"
-import { BsFillPersonLinesFill } from "react-icons/bs"
 
 export default function SocialLinks() {
   const socials = [
@@ -40,6 +39,7 @@ export default function SocialLinks() {
       <ul>
         {socials.map(({ id, child, url, style }) => (
           <li
+            key={id}
             className={`flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 ${style}`}
           >
             <a
