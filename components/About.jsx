90aa65e14bf0c67b8e6e9dd36473830ac283bf48
Pau-Marin <ritsu95@icloud.com>
2023-04-09 13:41:06 +0200

    Update About.jsx
    Added margint at the top to avoid the title ending behind the menu.

--- components/About.jsx
+++ components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
       name="about"
       className="w-full min-h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-auto">
+      <div className="max-w-screen-lg mt-20 p-4 mx-auto flex flex-col justify-center w-full h-auto">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
