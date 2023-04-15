a3f559a42c86b180b8d3354de7d01381ec52d6f1
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2023-04-15 18:01:01 +0200

    Update Contact.tsx
    Added basic messages for required fields

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,6 +6,22 @@ const initValues = { name: '', email: '', message: '' }
 
 const Contact = () => {
   const [contact, setContact] = useState(initValues)
+  const [touched, setTouched] = useState({
+    name: false,
+    email: false,
+    message: false,
+  })
+
+  const onBlur = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) =>
+    setTouched({
+      ...touched,
+      [e.target.name]: true,
+    })
+
   const handleChange = (
     e:
       | React.ChangeEvent<HTMLInputElement>
@@ -43,27 +59,39 @@ const Contact = () => {
               placeholder="Name*"
               value={contact.name}
               onChange={handleChange}
+              onBlur={onBlur}
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
               required
             ></input>
+            {touched.name && !contact.name && (
+              <p className="text-red-600">Required</p>
+            )}
             <input
               type="email"
               name="email"
               placeholder="Your email*"
               value={contact.email}
               onChange={handleChange}
+              onBlur={onBlur}
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
               required
             ></input>
+            {touched.email && !contact.email && (
+              <p className="text-red-600">Required</p>
+            )}
             <textarea
               name="message"
               placeholder="I want to talk about..."
               value={contact.message}
               onChange={handleChange}
+              onBlur={onBlur}
               rows={10}
               className="p-1 bg-transparent border-2 rounded-md text-white focus:outline-none"
               required
             ></textarea>
+            {touched.message && !contact.message && (
+              <p className="text-red-600">Required</p>
+            )}
             <p className="text-neutral-500">* Required fields.</p>
             <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Send!
